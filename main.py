99f6b3ba0ba2ce33a7142db38cf855bf0974035c
Mechanic <gazizovrustam.r@gmail.com>
2022-06-08 18:43:47 +0300

    Авторизация и переход на вкладку Средства ТУ

--- main.py
+++ main.py
@@ -16,30 +16,25 @@ def main():
     buttonTools = '//*[@id="menu"]/ul/li[3]/div[1]'
     buttonToolsList = '//*[@id="menu"]/ul/li[3]/ul/li[2]/a'
 
-    autorisation(login, password, boxLogin, boxPassword)
+    """Авторизация"""
+    authorisation(login, password, boxLogin, boxPassword, buttonEnter)
+
+    """Ожидаем прогрузки страницы и переход на вкладку Средства ТУ"""
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/header/nav/ul/li[3]/div[2]')))
+    finally:
+        clickButton(seearchBox(buttonTools))
+        clickButton(seearchBox(buttonToolsList))
+        time.sleep(5)
+        driver.quit()
 
 
-
-def autorisation(login, password, lineAddressLogin, lineAddressPassword):
-
-    # """Найти поле ввода логин и пароль"""
-    # inputBoxLogin = seearchBox(lineAddressLogin)
-    # inputBoxPassword = seearchBox(lineAddressPassword)
-
-    """Ввести логин и пароль"""
+def authorisation(login, password, lineAddressLogin, lineAddressPassword, lineAddressEnter):
+    """Ввести логин, пароль и "Войти" """
     sendKeys(seearchBox(lineAddressLogin), login)
     sendKeys(seearchBox(lineAddressPassword), password)
-
-
-    # """Нажать кнопку "Войти"""""
-    # clickEnter = seearchBox()
-    #
-    #
-    #
-    #
-    # seearchButton = driver.find_element(By.XPATH, buttonEnter)
-    # seearchButton.click()
-
+    clickButton(seearchBox(lineAddressEnter))
 
 
 def seearchBox(path):
@@ -55,23 +50,4 @@ def clickButton(button):
     button.click()
 
 
-
-
-    # seearchBoxPassword = driver.find_element(By.XPATH, boxPassword)
-    # seearchBoxPassword.send_keys(password)
-    # seearchButton = driver.find_element(By.XPATH, '//*[@id="formTab"]/div[3]/button')
-    # seearchButton.click()
-
-
-
-# try:
-#     element = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.XPATH, '/html/body/header/nav/ul/li[3]/div[2]')))
-# finally:
-#     seearchButtonStu = driver.find_element(By.XPATH, buttonTools)
-#     seearchButtonStu.click()
-#     searchButtonStuP = driver.find_element(By.XPATH, buttonToolsList)
-#     searchButtonStuP.click()
-#     time.sleep(3)
-#     driver.quit()
 main()
