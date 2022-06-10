4bb2daef9a13f258389abe6d0c243637d0bddc45
Mechanic <gazizovrustam.r@gmail.com>
2022-06-10 16:44:45 +0300

    Авторизация и переход в СТУ

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ driver.get('http://192.168.1.235/')
 def main():
 
 
-
+    buttonUser = ''
     buttonTools = '//*[@id="menu"]/ul/li[3]/div[1]'
     buttonToolsList = '//*[@id="menu"]/ul/li[3]/ul/li[2]/a'
     buttonThemeList = '//*[@id="menu"]/ul/li[4]/ul/li[2]/a'
@@ -18,18 +18,27 @@ def main():
     """Авторизация"""
     authorisation(login=login_password()[0], password=login_password()[1])
 
+    """ Переход на странице СТУ"""
+    click_button(buttonTools, buttonToolsList)
 
-def click_button(findButton, clickOneButton, ClickTwoButton):
+    """Переход на страницу Пользователи"""
+
+
+def click_button(findButton, clickButton):
+
     """Ожидаем прогрузки страницы и переход на вкладку"""
     try:
         element = WebDriverWait(driver, 10, 2).until(
             EC.visibility_of_element_located((By.XPATH, findButton))
         )
         driver.find_element(By.XPATH, findButton).click()
-        driver.find_element(By.XPATH, clickTwoButton).click()
+        driver.find_element(By.XPATH, clickButton).click()
     except:
         driver.quit()
         print("Обьект не найден!",  findButton)
+
+
+
 
 
 def authorisation(login, password):
