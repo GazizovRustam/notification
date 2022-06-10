4e1736c71b0338e9d40e02d9ed43b688dd44b572
Mechanic <gazizovrustam.r@gmail.com>
2022-06-10 13:15:46 +0300

    Авторизация

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ driver = webdriver.Chrome()
 driver.get('http://192.168.1.235/')
 
 def main():
-    login = 'Otv_O_ShCh1_1'
-    password = '123123Aa'
+
+
 
     buttonTools = '//*[@id="menu"]/ul/li[3]/div[1]'
     buttonToolsList = '//*[@id="menu"]/ul/li[3]/ul/li[2]/a'
+    buttonThemeList = '//*[@id="menu"]/ul/li[4]/ul/li[2]/a'
 
     """Авторизация"""
-    authorisation(login, password)
+    authorisation(login=login_password()[0], password=login_password()[1])
 
-    """Ожидаем прогрузки страницы и переход на вкладку Средства ТУ"""
+
+def click_button(findButton, clickOneButton, ClickTwoButton):
+    """Ожидаем прогрузки страницы и переход на вкладку"""
     try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/header/nav/ul/li[3]/div[2]')))
-    finally:
-        driver.find_element(By.XPATH, buttonTools).click()
-        driver.find_element(By.XPATH, buttonToolsList).click()
-        time.sleep(5)
+        element = WebDriverWait(driver, 10, 2).until(
+            EC.visibility_of_element_located((By.XPATH, findButton))
+        )
+        driver.find_element(By.XPATH, findButton).click()
+        driver.find_element(By.XPATH, clickTwoButton).click()
+    except:
         driver.quit()
+        print("Обьект не найден!",  findButton)
 
 
 def authorisation(login, password):
@@ -37,6 +41,16 @@ def authorisation(login, password):
     driver.find_element(By.XPATH, boxLogin).send_keys(login)
     driver.find_element(By.XPATH, boxPassword).send_keys(password)
     driver.find_element(By.XPATH, buttonEnter).click()
+
+
+def login_password():
+    login = 'Otv_O_ShCh1_1'
+    password = '123123Aa'
+    return login, password
+
+
+# def create_user():
+#     buttonUser = '//*[@id="menu"]/ul/li[2]'
 
 
 main()
