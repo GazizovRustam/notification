e0dddcd3107dc4eaec04b6e58201910c8c044097
Mechanic <85064228+GazizovRustam@users.noreply.github.com>
2022-06-13 09:51:02 +0300

    json 2.0

--- openFile/workbook.py
+++ openFile/workbook.py
@@ -2,29 +2,28 @@ from openpyxl import load_workbook
 import json
 
 
-book = load_workbook('Meta.xlsx')
-sheet = book.active
-rows = sheet.rows
-sheets = book.sheetnames[0]
+def get_json(path):
+    book = load_workbook(path)
+    sheet = book.active
+    rows = sheet.rows
+    sheets = book.sheetnames[0]
 
-headers = [cell.value for cell in next(rows)]
+    headers = [cell.value for cell in next(rows)]
 
-all_rows = []
+    all_rows = []
 
-persons = {
-    sheets: all_rows
-}
+    persons = {
+        sheets: all_rows
+    }
 
-for row in rows:
-    date = {}
-    for title, cell in zip(headers, row):
-        date[title] = cell.value
-    all_rows.append(date)
+    for row in rows:
+        date = {}
+        for title, cell in zip(headers, row):
+            date[title] = cell.value
+        all_rows.append(date)
 
-print(all_rows)
-
-with open('persons.json', 'w') as file:
-    json.dump(persons, file, indent=2, ensure_ascii=False)
+    with open('persons.json', 'w') as file:
+        json.dump(persons, file, indent=2, ensure_ascii=False)
 
 
 
