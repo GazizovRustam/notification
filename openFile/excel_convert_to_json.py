7b9bceb759906abbbf2a8ac0e07b525e67fc527d
Mechanic <85064228+GazizovRustam@users.noreply.github.com>
2022-06-14 10:04:32 +0300

    json 2.1

--- openFile/excel_convert_to_json.py
+++ openFile/excel_convert_to_json.py
@@ -2,30 +2,29 @@ from openpyxl import load_workbook
 import json
 
 
-# def get_json(path):
-book = load_workbook('Meta.xlsx')
-sheet = book.active
-rows = sheet.rows
-sheets = book.sheetnames[0]
+def get_json(path):
+    book = load_workbook('Meta.xlsx')
+    sheet = book.active
+    rows = sheet.rows
+    sheets = book.sheetnames[0]
 
-headers = [cell.value for cell in next(rows)]
+    headers = [cell.value for cell in next(rows)]
 
-all_rows = []
+    all_rows = []
 
-persons = {
-    sheets: all_rows
-}
+    persons = {
+        sheets: all_rows
+    }
 
-for row in rows:
-    date = {}
-    for title, cell in zip(headers, row):
-        date[title] = cell.value
-    all_rows.append(date)
+    for row in rows:
+        date = {}
+        for title, cell in zip(headers, row):
+            date[title] = cell.value
+        all_rows.append(date)
 
 
-with open(sheets, 'w', encoding='utf8') as file:
-    json.dump(persons, file, indent=2, ensure_ascii=False)
+    with open(sheets, 'w', encoding='utf8') as file:
+        json.dump(persons, file, indent=2, ensure_ascii=False)
 
-    # return persons
 
-# get_json(r'C:\Users\GRX\Desktop\Meta.xlsx')
+get_json(r'C:\Users\GRX\Desktop\Meta.xlsx')
